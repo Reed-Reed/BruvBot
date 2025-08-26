a5a15403e62b69848afca2748486ad9db0179890
Reed-Reed <146047315+Reed-Reed@users.noreply.github.com>
2025-08-26 02:41:55 -0400

    Update BruvBot.py

--- BruvBot.py
+++ BruvBot.py
@@ -1,89 +1,65 @@
-"""
-Discord Bot
-Author: Reed Murray
-
-This program joins a discord bot to the bruv village discord server. It uses asynchronous functions to
-respond to messages and track data.
-
-Current features:
-    - Movie list: tracks a list of movies to watch in the discord chat together
-"""
+#!/usr/bin/python3
 
 import discord
+from discord.ext import tasks, commands
+import asyncio
 
-# Setup for the discord connection and permissions.
+# Configure Bot
 intents = discord.Intents.default()
 intents.message_content = True
+Bot = commands.Bot(command_prefix="bruv ", intents=intents)
 
-
-client = discord.Client(intents=intents)
-
-# Provides indication in the console that the bot is connected.
-@client.event
+@Bot.event
 async def on_ready():
-    print(f'{client.user} is operational')
+    print(f'{Bot.user} is online.')
 
-""" 
-The following section is the Movie list tracker. 
-The first branch adds a movie when '$addmovie' is the message prefix. It adds a movie to the list.
-The file is kept in the format of movie names separated by newlines. 
-"""
-@client.event
+# Create the movie file if it doesn't exit yet
+try:
+    with open('movie_list.txt', 'x') as mf:
+        pass
+except FileExistsError:
+    print("movie_list file already exists")
+
+@Bot.event
 async def on_message(message):
-    if message.author == client.user:   # This ensures the bot doesn't respond to itself
+    # Avoid bot responding to itself
+    if message.author == Bot.user:
         return
+    # get the sender's nickname
+    display_name = message.author.display_name
 
-    movie_list = []
+    if "payday" in message.content.lower():
+        await message.channel.send(
+            f"Ayo {display_name} I heard payday had an update JK GO TOUCH GRASS"
+            )
+    # continue processing commands
+    await Bot.process_commands(message)
 
-    if message.content.startswith('$addmovie'):   # populate list with movie names from file
-        with open('movie_list.txt', 'r') as mf:
-            for line in mf:
-                movie_list.append(line.strip())
-
-        movie = message.content[10:]   # parse movie name and reference against list to avoid duplicates
-        print(movie)
-        if movie in movie_list:
-            return
-
-        with open('movie_list.txt', 'a') as mf:     # add the movie to the end of the file, then a newline.
-            mf.write(movie)
-            mf.write('\n')
-
-        await message.channel.send('Added to list')
-
-# This branch deletes a movie when prefixed with $deletemovie.
-    if message.content.startswith('$deletemovie'):
-        with open('movie_list.txt', 'r') as mf:
-            movie_list = []
-            for line in mf:
-                movie_list.append(line.strip())
-
-        print(movie_list)
-
-        movie = message.content[13:]     # parse movie name and reference against list to avoid duplicates
-        if movie not in movie_list:
-            return
+#################################
+#     Movie List Commands       #
+#################################
+@Bot.command()
+async def addmovie(ctx, *, movie: str):
+    with open('movie_list.txt', 'a+') as mf:
+        mf.seek(0)
+        mf_content = mf.read()
+        if movie.lower() in mf_content:
+            await ctx.send(f"```{movie} is already in the list```")
         else:
-            movie_list.remove(movie)
+            mf.write(movie.lower())
+            mf.write("\n")
+            await ctx.send('```Movie added to List```')
 
-        with open('movie_list.txt', 'w') as mf:
-            for movie in movie_list:
-                mf.write(movie)
-                mf.write('\n')
+@Bot.command()
+async def showmovies(ctx):
+    with open('movie_list.txt', 'r') as mf:
+        mf.seek(0)
+        mf_content = mf.read()
+        await ctx.send("Complete list of movies:\n" + mf_content)
 
-        await message.channel.send('Deleted from list')
-
-# This branch lists the movies via message in discord chat. The message is in a crude format currently.
-    if message.content.startswith('$movielist'):
-        with open('movie_list.txt', 'r') as mf:
-            movie_list = []
-            for line in mf:
-                movie_list.append(line.strip('\n'))
-
-        embed = discord.Embed(title='Movies', description='Movie list', color=0x00ff00)
-
-        embed.add_field(name='Movies', value=movie_list, inline=False)
-
-        await message.channel.send(embed=embed)  # TODO: format this to look like a cleaner and organized list
-
-client.run('MTE4MjM4NjE2ODgzMTE2NDUxNw.GO7pnx.qIe2MeRRbonRNqFB_1uBKvcYw41CQcd5mVKhlk')
+@Bot.command()
+async def deletemovie(ctx, *, movie: str):
+    with open('movie_list.txt', 'w+') as mf:
+        movie_list = [line for line in mf if line != movie.lower() + "\n"]
+        mf.writelines(movie_list)
+    await ctx.send(f"{movie} deleted from list")
